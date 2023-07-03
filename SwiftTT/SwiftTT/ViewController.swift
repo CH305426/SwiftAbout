971532d825f361a7889b028ffeefb738776588db
lijunqinng <860010082@qq.com>
2023-07-04 00:18:34 +0800

    初始化

---
+++ SwiftTT/SwiftTT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTT
+//
+//  Created by Junq on 2023/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
